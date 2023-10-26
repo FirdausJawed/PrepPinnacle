e90aeeadd99d4041dc6719bbedf7be0afb7bdd36
Firdaus Jawed <85278666+FirdausJawed@users.noreply.github.com>
2023-10-27 00:29:52 +0530

    created the card component

--- knowledge_wave/src/App.js
+++ knowledge_wave/src/App.js
@@ -1,23 +1,27 @@
-// App.js
 import React from 'react';
 import './App.css';
-import ProfileHeader from './ProfileHeader';
-import ProfileDetails from './ProfileDetails';
+import Card from './card';
 
 function App() {
   const user = {
     name: 'Firdaus Jawed',
     bio: 'Student',
     location: 'Patna, Bihar, India',
+    status:'Knight',
     email: 'firdausjawed01@gmail.com',
     profilePicture: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwH5hDDnbNvivR5amzhoou3ueiy1E7dursMQ&usqp=CAU',
   };
 
   return (
     <div className="App">
-      <ProfileHeader user={user} />
-      <ProfileDetails user={user} />
+      <div className='card'>
+        <Card user={user} />
+      </div>
+      
     </div>
+
+    
+
   );
 }
 
